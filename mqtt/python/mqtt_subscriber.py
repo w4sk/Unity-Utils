de417384932e5e89758b25b869b29e5151607afb
w4sk <guiguidubian@gmail.com>
2024-02-02 22:01:38 +0900

    Refactored MQTT sub functionality into a class structure

--- mqtt/python/mqtt_subscriber.py
+++ mqtt/python/mqtt_subscriber.py
@@ -3,42 +3,43 @@ import random
 from paho.mqtt import client as mqtt_client
 
 
-broker = 'localhost'
-port = 1883
-topic = "python/mqtt"
-# generate client ID with pub prefix randomly
-client_id = f'python-mqtt-{random.randint(0, 100)}'
-# username = 'emqx'
-# password = 'public'
-
-
-def connect_mqtt() -> mqtt_client:
+class MQTTSubscriber:
+  def __init__(self, broker='localhost', port=1883, topic="python/mqtt"):
+    self.broker = broker
+    self.port = port
+    self.topic = topic
+    self.client_id = f'python-mqtt-{random.randint(0, 100)}'
+    # self.username = 'emqx'
+    # self.password = 'public'
+    self.client = self.connect_mqtt()
+  
+  def connect_mqtt(self) -> mqtt_client:
     def on_connect(client, userdata, flags, rc):
-        if rc == 0:
-            print("Connected to MQTT Broker!")
-        else:
-            print("Failed to connect, return code %d\n", rc)
-
-    client = mqtt_client.Client(client_id)
-    # client.username_pw_set(username, password)
+      if rc == 0:
+        print("Connected to MQTT Broker!")
+      else:
+        print("Failed to connect, return code %d\n", rc)
+    
+    client = mqtt_client.Client(self.client_id)
+    # client.username_pw_set(self.username, self.password)
     client.on_connect = on_connect
-    client.connect(broker, port)
+    client.connect(self.broker, self.port)
     return client
-
-
-def subscribe(client: mqtt_client):
+  
+  def subscribe(self):
     def on_message(client, userdata, msg):
-        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
-
-    client.subscribe(topic)
-    client.on_message = on_message
-
-
-def run():
-    client = connect_mqtt()
-    subscribe(client)
-    client.loop_forever()
-
+      try:
+        print(f"Received `{msg.payload.decode('utf-8')}` from `{msg.topic}` topic")
+      except UnicodeDecodeError:
+        print("Received message with non-utf-8 encoded payload")
+    
+    self.client.subscribe(self.topic)
+    self.client.on_message = on_message
+  
+  def run(self):
+    self.subscribe()
+    self.client.loop_forever()
 
 if __name__ == '__main__':
-    run()
+  subscriber = MQTTSubscriber()
+  subscriber.run()
